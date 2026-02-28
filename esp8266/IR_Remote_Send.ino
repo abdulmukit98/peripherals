5f8083b87be1d9a5d0254a86c78f4b83372401a3
Abdul Mukit <mdabdulmukit98@gmail.com>
2026-02-28 19:36:42 +0600

    Update IR_Remote_Send.ino

--- esp8266/IR_Remote_Send.ino
+++ esp8266/IR_Remote_Send.ino
@@ -11,12 +11,15 @@ void setup() {
   Serial.println("IR Transmitter Ready...");
 }
 
+// 0x0x3D00FF OFF
+// 0x0x3D807F Mute
+
 void loop() {
 
   Serial.println("Sending IR Code...");
   
   // Send NEC code
-  irsend.sendNEC(0x3D30CF, 32);  // 32 bits for NEC
+  irsend.sendNEC(0x3D00FF, 32);  // 32 bits for NEC
   
-  delay(5000);  // send every 5 seconds
+  delay(2000);  // send every 5 seconds
 }
